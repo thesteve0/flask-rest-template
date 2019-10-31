a8b5c58a0fe3b386d247d84c1ef009b266b33ea0
steve pousty <steve.pousty@gmail.com>
2019-10-31 12:24:02 -0700

    cleanup

--- wsgi.py
+++ wsgi.py
@@ -2,7 +2,6 @@ import os
 import json
 
 from flask import Flask
-from flask import render_template
 
 # Setup the Flask application.
 
@@ -10,11 +9,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    title = os.environ.get('HOMEROOM_TITLE', 'Workshops')
-    branding = os.environ.get('HOMEROOM_BRANDING', 'openshift')
-    banner_image = banner_images.get(branding, banner_images['openshift'])
-
-    visible_workshops = list(filter_out_hidden(workshops))
-
-    return render_template('workshops.html', title=title,
-            banner_image=banner_image, workshops=visible_workshops)
+    return "hello world"
